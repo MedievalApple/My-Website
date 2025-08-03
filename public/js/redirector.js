6119410f90c819fe69eedc1281e380c65ca18930
Medieval Apple <114710036+MedievalApple@users.noreply.github.com>
2025-08-03 01:17:25 -0400

    Fix 404 redirect

--- public/js/redirector.js
+++ public/js/redirector.js
@@ -17,7 +17,7 @@ if(document.location.pathname == "/404.html" | document.location.pathname == "/4
 }
 else{
     try {
-        path = document.location.pathname.split(".")[0].toLowerCase()
+        path = document.location.pathname.split(".")[0].toLowerCase().replace(/\/\s*$/, "")
         if(redirectTable[path] != undefined){
             document.location.pathname = redirectTable[path]
         } 
