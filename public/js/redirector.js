1ed1ef8013dac0e3466ac51e213b41a3e42b070b
Medieval Apple <evergaming2500@gmail.com>
2026-01-18 10:48:36 -0500

    Fix 404 redirector

--- public/js/redirector.js
+++ public/js/redirector.js
@@ -2,16 +2,16 @@ topNav = document.getElementById("topnav")
 errorWindow = document.getElementById("window")
 
 const redirectTable = {
-        "/Media-Assistant": "/media-assistant.html",
+        "/media-assistant": "/media-assistant.html",
         "/ma": "/media-assistant.html",
         "/ma-setup": "/media-assistant/setup.html",
         "/public/licenses/media-assistant-license": "/media-assistant/license.txt",
         "/ma-privacy-information": "/media-assistant/privacy-info.html",
-        "/Rally-Waffle": "/rally-waffle.html",
+        "/rally-waffle": "/rally-waffle.html",
         "/rw": "/rally-waffle.html",
-        "/Legend-of-Sprig": "/legend-of-sprig.html",
+        "/legend-of-sprig": "/legend-of-sprig.html",
         "/los": "/legend-of-sprig.html",
-        "/About": "/about.html"
+        "/about": "/about.html"
     }
 
 if(document.location.pathname == "/404.html" | document.location.pathname == "/404"){
